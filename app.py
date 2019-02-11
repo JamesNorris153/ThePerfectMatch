b016c25d004a6f0554fc5aa86ee365496ef05c76
Matthew Wooding <matthew.wooding@me.com>
2019-02-11 12:41:52 +0000

    Initialised 'empty' project

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+#Example for importing methods
+#from models import get_users, create_user, get_user
+
+app = Flask(__name__, static_url_path='/static')
+
+CORS(app)
+
+## Sends static files when necessary
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static',path)
+
+## Landing page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+## Internal server error
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html")
+
+## Page not found
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,28 @@
+import sqlite3 as sql
+from os import path
+
+ROOT = path.dirname(path.relpath((__file__)))
+
+# Example Methods
+# def create_user(email,name):
+#     con = sql.connect(path.join(ROOT, 'database.db'))
+#     cur = con.cursor()
+#     cur.execute('INSERT INTO users (email,name) VALUES (?,?)',(email,name))
+#     con.commit()
+#     con.close()
+#
+# def get_users():
+#     con = sql.connect(path.join(ROOT, 'database.db'))
+#     cur = con.cursor()
+#     cur.execute('SELECT * FROM users')
+#     users = cur.fetchall()
+#     con.close()
+#     return users
+#
+# def get_user(id):
+#     con = sql.connect(path.join(ROOT, 'database.db'))
+#     cur = con.cursor()
+#     cur.execute('SELECT * FROM users WHERE id=?',(id))
+#     user = cur.fetchall()
+#     con.close()
+#     return user
