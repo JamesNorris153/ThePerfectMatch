fce6aa84aa796d204445429232745ce997e7763b
Shen Yiang <shen980630@gmail.com>
2019-03-01 17:44:19 +0000

    Upload New File

---
+++ functions/main.m
@@ -0,0 +1,25 @@
+function [score2] = main(X, y, m, X2)
+    %% Setup the parameters
+    num_labels = 2;          % 2 labels indicate the result can be either 0 or 1 
+
+    %data = load('score.txt');
+    %X = data(:, 1:end - 1);
+    %y = data(:, end);
+    X = double(cell2mat(X));
+    y = double(cell2mat(y));
+    X = reshape(X, [m, size(X, 2) / m]);
+
+    y = reshape(y, [m, 1]);
+    %m = size(X, 1);
+
+    %% Apply linear regression
+    lambda = 0.5;
+    [all_theta] = oneVsAll(X, y, num_labels, lambda);
+
+    %% Prediction
+    X2 = double(cell2mat(X2));
+    X2 = reshape(X2, [m, size(X2, 2) / m]);
+    score2 = predictOneVsAll(all_theta, X2);
+    pred2 = round(score2);
+
+end
