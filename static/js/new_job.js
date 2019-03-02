b2a33caee44504cd34b5f5e6a7b6b1d05fba1a0b
Matthew Wooding <matthew@Matthews-Mac-mini.local>
2019-03-02 17:36:17 +0000

    Implemented choosing number of questions to give applicants

--- static/js/new_job.js
+++ static/js/new_job.js
@@ -15,12 +15,6 @@ function saveChanges() {
     $('.job_description_box').addClass('is-danger');
   }
 
-  // If there have been any errors thus far, don't try parsing data
-  if ($('.job_item:not(.template) .is-danger:not(.button)').length != 0) {
-    showJobLoadingModal('Failure','Some details were not entered correctly, please see the red boxes');
-    return;
-  }
-
   // Get job properties
   job_name = $('#job_details input[name="job_name"]').val();
   job_position = $('#job_details div[name="job_position"] select option:selected').val();
@@ -29,23 +23,34 @@ function saveChanges() {
   job_description = $('.job_description_box').val();
 
   // Error Check Deadline
-  var t = job_deadline.match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
+  var t = $('#job_details input[name="job_deadline"]').val().match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
   if(t !== null){
     var d = +t[1], m = +t[2], y = +t[3];
     var date = new Date(y, m - 1, d);
     if(date.getFullYear() === y && date.getMonth() !== m - 1) {
       $('#job_details input[name="job_deadline"]').addClass('is-danger');
-      showJobLoadingModal('Failure','The deadline entered was in an invalid format.<br>Please use dd/mm/yyyy');
-      return;
     }
   } else {
     $('#job_details input[name="job_deadline"]').addClass('is-danger');
-    showJobLoadingModal('Failure','The deadline entered was in an invalid format.<br>Please use dd/mm/yyyy');
+  }
+
+  all_questions = $('.test_question:not(.template)');
+
+  // Get number of questions to randomly ask applicant
+  question_number = $('.question_number').val();
+  if (isNaN(question_number)) {
+    $('.question_number').addClass('is-danger');
+  } else if (question_number < 0 || question_number > $(all_questions).length) {
+    $('.question_number').addClass('is-danger');
+  }
+
+  // If there have been any errors thus far, don't try parsing data
+  if ($('.job_item:not(.template) .is-danger:not(.button)').length != 0) {
+    showJobLoadingModal('Failure','Some details were not entered correctly, please see the red boxes');
     return;
   }
 
   // Get all questions
-  all_questions = $('.test_question:not(.template)');
   question_details = []
   $(all_questions).each(function() {
     question = $(this).find('input[name="question"]').val();
@@ -75,12 +80,12 @@ function saveChanges() {
     "Location":job_location,
     "Position":job_position,
     "Status":job_status,
-    "Questions":question_details
+    "Questions":question_details,
+    "QuestionNumber":question_number
   };
 
   var job_id = $('#cur_job_id').html();
 
-  // showJobLoadingModal('Success',JSON.stringify(job));
   $.post('/staff/save_job', {job_id:job_id, job: JSON.stringify(job)}, function(data) {
     if (data == "Success") {
       showJobLoadingModal("Success","All Changes Saved");
